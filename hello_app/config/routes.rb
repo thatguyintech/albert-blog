bc9eb314cdadda48809b5e240ce436dcaea3273b
Albert Hu <alberthu@berkeley.edu>
2016-06-06 14:59:08 -0700

    Initialize repository

--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -48,7 +48,7 @@ HelloApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root 'application#hello'
+  root :to => 'application#hello'
 
   # See how all your routes lay out with "rake routes"
 
