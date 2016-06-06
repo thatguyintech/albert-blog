fe642c5b5e465aba0725a97b6cc7c11782e526ae
Albert Hu <alberthu@berkeley.edu>
2016-06-06 14:49:25 -0700

    Start tutorial

---
+++ hello_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloApp::Application.config.secret_token = '1d11bb93137742c3a6a56e1d4263a17b884eb8b0e66f460c0894b3dddf3ee332e3884cb241a12e5bc56c3355dfe6a9c7913a34fcf82582696d9234af110cf561'
